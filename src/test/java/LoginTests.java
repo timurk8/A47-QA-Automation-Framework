07e0a31d954422a203834ef5042fdb04ebd83bd3
timurk8 <136999705+timurk8@users.noreply.github.com>
2023-06-20 23:56:38 +0300

    HEY

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -21,7 +21,7 @@ public class LoginTests extends BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        System.out.println("TEST---111222");
+        System.out.println("TEST-HEY--111222");
         driver.quit();
     }
 }
