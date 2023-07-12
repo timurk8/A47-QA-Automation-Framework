fc263a992ee55eca0aeaad2dff08b3425571f8bf
timurk8 <136999705+timurk8@users.noreply.github.com>
2023-07-12 16:42:04 +0300

    Homework22

--- src/test/java/Draft.java
+++ src/test/java/Draft.java
@@ -11,32 +11,14 @@ import java.util.List;
 
 public class Draft extends BaseTest {
 
-    @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
-    public void loginValidEmailPasswordTest(){
+    @Test(enabled = true, priority = 1, description = "Login with valid email and valid password")
+    public void loginValidEmailPasswordTest() {
 
         navigateToPage();
         provideEmail("bugbusters@testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
 
-        System.out.println(checkPlaylistsExist());
 
     }
-
-
-//    public static boolean checkPlaylistsExist() {
-//        try {
-//            List<WebElement> sidebarPlayLists = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id='playlists']/ul/li[3]/a")));
-//            return !sidebarPlayLists.isEmpty();
-//        } catch (TimeoutException e) {
-//            return false;
-//        }
-//    }
-
-    //Check if Playlists exist
-    protected static boolean checkPlaylistsExist() {
-        List<WebElement> sidebarPlayLists = driver.findElements(By.xpath("//section[@id='playlists']/ul/li[3]"));
-        return !sidebarPlayLists.isEmpty();
-    }
-
 }
